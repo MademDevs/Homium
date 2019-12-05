b213f30a55aa0a617e5b7c14e6b85b34c86d6d0b
andreklein <andre.klein@hof-university.de>
2019-12-05 11:26:23 +0100

    implemented recipe layout still to do: Database connection Add/edit-Screen

--- app/src/main/java/de/madem/homium/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/de/madem/homium/ui/fragments/recipes/RecipesFragment.kt
@@ -1,18 +1,24 @@
 package de.madem.homium.ui.fragments.recipes
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import de.madem.homium.R
+import de.madem.homium.managers.adapters.RecipesListAdapter
 
 class RecipesFragment : Fragment() {
 
     private lateinit var recipesViewModel: RecipesViewModel
+    private lateinit var root: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,11 +27,25 @@ class RecipesFragment : Fragment() {
     ): View? {
         recipesViewModel =
             ViewModelProviders.of(this).get(RecipesViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_recipes, container, false)
-        val textView: TextView = root.findViewById(R.id.text_recipes)
-        recipesViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        root = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        registerRecyclerView()
+
         return root
     }
+
+    private fun registerRecyclerView() {
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerView_recipes)
+        if(activity?.resources?.configuration?.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            recyclerView.layoutManager = GridLayoutManager(context, 2)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(context, 3)
+        }
+        val adapter = RecipesListAdapter(this, recipesViewModel.recipeList)
+        recyclerView.adapter = adapter
+        adapter.shortClickListener = {recipe, viewHolder ->
+            Toast.makeText(context, "Selected ${recipe.name}", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
--- app/src/main/java/de/madem/homium/ui/fragments/recipes/RecipesViewModel.kt
+++ app/src/main/java/de/madem/homium/ui/fragments/recipes/RecipesViewModel.kt
@@ -3,11 +3,19 @@ package de.madem.homium.ui.fragments.recipes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import de.madem.homium.models.Recipe
+import de.madem.homium.models.ShoppingItem
 
 class RecipesViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "Die Rezeptverwaltung ist bald verfügbar ;)"
+    val recipeList = MutableLiveData<List<Recipe>>().apply {
+        value = listOf(
+            Recipe("rezept1", "beschreibung1", "bild1"),
+            Recipe("rezept2", "beschreibung2", "bild2"),
+            Recipe("rezept3", "beschreibung3", "bild3"),
+            Recipe("rezept4", "beschreibung4", "bild4"),
+            Recipe("rezept5", "beschreibung5", "bild5"),
+            Recipe("rezept6", "beschreibung6", "bild6"),
+            Recipe("rezept7", "beschreibung7", "bild7")
+            )
     }
-    val text: LiveData<String> = _text
 }
