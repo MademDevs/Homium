413a9cf889e5207c2f9a72e900f24c04745dd934
Max <development.maax.gr>
2019-12-15 14:57:27 +0100

    inventoy item add fertig

--- app/src/main/java/de/madem/homium/ui/fragments/inventory/InventoryFragment.kt
+++ app/src/main/java/de/madem/homium/ui/fragments/inventory/InventoryFragment.kt
@@ -8,15 +8,20 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import de.madem.homium.R
+import de.madem.homium.constants.REQUEST_CODE_SHOPPING
 import de.madem.homium.managers.adapters.InventoryItemListAdapter
 import de.madem.homium.ui.activities.inventoryedit.InventoryItemEditActivity
+import de.madem.homium.ui.activities.shoppingitem.ShoppingItemEditActivity
 import de.madem.homium.utilities.switchToActivity
+import de.madem.homium.utilities.switchToActivityForResult
 
 class InventoryFragment : Fragment() {
 
     //private lateinit var binding: ResultPro
     private lateinit var inventoryViewModel: InventoryViewModel
+    private lateinit var root: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,12 +29,24 @@ class InventoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         inventoryViewModel = ViewModelProviders.of(this).get(InventoryViewModel::class.java)
+        root = inflater.inflate(R.layout.fragment_inventory, container, false)
 
-        return inflater.inflate(R.layout.fragment_inventory, container, false)
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_inventory)
+        registerRecyclerView()
+        registerFloatingActionButton()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        inventoryViewModel.reloadInventoryItems()
+    }
+
+    private fun registerRecyclerView() {
+        val recyclerView = root.findViewById<RecyclerView>(R.id.rv_inventory)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
         val inventoryAdapter = InventoryItemListAdapter(this, inventoryViewModel.inventoryItems)
@@ -41,10 +58,15 @@ class InventoryFragment : Fragment() {
         recyclerView.adapter = inventoryAdapter
     }
 
-    override fun onResume() {
-        super.onResume()
 
-        inventoryViewModel.reloadInventoryItems()
+    private fun registerFloatingActionButton() {
+        //floating action button
+        val btnAddShoppingItem = root.findViewById<FloatingActionButton>(R.id.fab_add_inventory)
+
+        btnAddShoppingItem.setOnClickListener {
+            //implementing simple navigation to inventory item edit screen via intent
+            switchToActivityForResult(0, InventoryItemEditActivity::class)
+        }
     }
 
 
