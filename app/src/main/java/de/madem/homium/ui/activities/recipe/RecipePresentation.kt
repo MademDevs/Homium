d1ad49b857efb6a9581adc3ee17f225409c0f3ed
andreklein <andre.klein@hof-university.de>
2019-12-15 14:23:12 +0100

    basic layout todo: add data implementation

---
+++ app/src/main/java/de/madem/homium/ui/activities/recipe/RecipePresentation.kt
@@ -0,0 +1,19 @@
+package de.madem.homium.ui.activities.recipe
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import de.madem.homium.R
+
+class RecipePresentation: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_presentation)
+
+    }
+
+}
