99111afb0e8e2b7e0b6c50cb55ba0ef3ca148aef
Andre Klein <klein@mailbox.org>
2019-12-23 07:02:16 +0100

    implemented models, edited database and dao for recipe ingredients and descriptions

--- app/src/main/java/de/madem/homium/databases/RecipeDao.kt
+++ app/src/main/java/de/madem/homium/databases/RecipeDao.kt
@@ -4,6 +4,8 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import de.madem.homium.models.Recipe
+import de.madem.homium.models.RecipeDescription
+import de.madem.homium.models.RecipeIngredient
 import de.madem.homium.models.ShoppingItem
 
 @Dao
@@ -21,7 +23,24 @@ interface RecipeDao {
     @Query("SELECT * FROM recipe WHERE uid = :id")
     fun getRecipeById(id: Int): Recipe
 
-    //TODO: Queries for Recipe and Ingredients!
+    @Insert
+    fun insertIngredient(vararg item: RecipeIngredient)
 
+    @Insert
+    fun insertDescription(vararg item: RecipeDescription)
+
+    @Query("SELECT * FROM ingredients WHERE recipeId = :recipeId")
+    fun getIngredientByRecipeId(recipeId: Int): List<RecipeIngredient>
+
+    @Query("SELECT * FROM descriptions WHERE recipeId = :recipeId")
+    fun getDescriptionByRecipeId(recipeId: Int): List<RecipeDescription>
+
+    @Query("DELETE FROM ingredients")
+    fun deleteAllIngredient()
+
+    @Query("DELETE FROM descriptions")
+    fun deleteAllDescription()
+
+    //TODO: Queries for Recipe and Ingredients!
 
 }
