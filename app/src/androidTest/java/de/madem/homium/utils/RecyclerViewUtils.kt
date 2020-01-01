a60a2124cbaa3ccfa867ab7df582b9f04d994b8b
Max <development.maax.gr>
2020-01-01 20:01:18 +0100

    die ersten beiden UI Tests implementiert

---
+++ app/src/androidTest/java/de/madem/homium/utils/RecyclerViewUtils.kt
@@ -0,0 +1,30 @@
+package de.madem.homium.utils
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.matcher.BoundedMatcher
+import org.hamcrest.Matcher
+
+
+object RecyclerViewUtils {
+
+    fun atPosition(position: Int, itemMatcher: Matcher<View>?): BoundedMatcher<View?, RecyclerView> {
+        checkNotNull(itemMatcher)
+
+        return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                        ?: // has no item on such position
+                        return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+
+            override fun describeTo(description: org.hamcrest.Description?) {
+                description?.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+        }
+    }
+
+}
