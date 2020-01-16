71f9059366edee8022338fd53cd250981b4de6e2
Marvin <mjakob@hof-university.de>
2020-01-16 11:31:11 +0100

    changed logo svg

--- app/src/main/java/de/madem/homium/ui/activities/launch/LaunchActivity.kt
+++ app/src/main/java/de/madem/homium/ui/activities/launch/LaunchActivity.kt
@@ -1,6 +1,5 @@
 package de.madem.homium.ui.activities.launch
 
-import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
